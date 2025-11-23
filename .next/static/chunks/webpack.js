240286c89bea5ea6a4606bbbefa2ea792a569645
Enrique G Zbinden <enriquezbinden04@gmail.com>
2025-11-23 02:03:22 +0100

    Refactor code structure for improved readability and maintainability

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6484b8d9ef6ba869"; }
+/******/ 		__webpack_require__.h = function() { return "f4312774555af202"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
